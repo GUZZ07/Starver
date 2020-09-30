f7a11f231a5e2081d19388a48e66885050961eac
tofout@qq.com <tofout@qq.com>
2020-09-30 21:00:46 +0800

    Realm开始添加

---
+++ PlayerBoosts/Realms/SRealm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Starvers.PlayerBoosts.Realms
+{
+	public class SRealm<T> : StarverRealm
+		where T : IRealmShape, new()
+	{
+		private T shape;
+
+		protected int timer;
+
+		public SRealm()
+		{
+			shape = new T();
+		}
+
+		public override bool InRange(Entity entity)
+		{
+			return shape.InRange(entity);
+		}
+
+		public override bool Intersect(Entity entity)
+		{
+			return shape.Intersect(entity);
+		}
+
+		protected override void Initialize()
+		{
+			shape.Begin(this);
+		}
+
+		protected override void InternalUpdate()
+		{
+			shape.Update(timer);
+			timer++;
+		}
+	}
+}
---
+++ PlayerBoosts/Realms/StarverRealm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace Starvers.PlayerBoosts.Realms
+{
+	public abstract class StarverRealm : IRealm
+	{
+		public int? TimeLeft { get; protected set; }
+
+		public int? DefTimeLeft { get; set; }
+		public virtual Vector Center { get; set; }
+		public bool Active { get; set; }
+
+		public abstract bool InRange(Entity entity);
+		public abstract bool Intersect(Entity entity);
+
+		public virtual void Kill()
+		{
+			Active = false;
+		}
+
+		public virtual void Begin()
+		{
+			Active = true;
+			TimeLeft = DefTimeLeft;
+			Initialize();
+		}
+
+		public virtual void Update()
+		{
+			InternalUpdate();
+			UpdateTimeLeft();
+		}
+
+		protected StarverRealm()
+		{
+
+		}
+
+		protected void UpdateTimeLeft()
+		{
+			if (TimeLeft != null)
+			{
+				if (--TimeLeft == 0)
+				{
+					Kill();
+				}
+			}
+		}
+
+		protected abstract void Initialize();
+		protected abstract void InternalUpdate();
+	}
+}
